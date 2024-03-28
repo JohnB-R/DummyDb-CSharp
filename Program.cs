ae0f5d14f9d6aad5d55b395178b8082701cf577a
JohnB-R <john@boyd-rainey.de>
2024-03-28 10:37:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running version <You think of a number>");
+        SqlRunner sqlRunner = new SqlRunner();
+        sqlRunner.RunSql(); // We want to make RunSql WRITE
+
+        Console.WriteLine("...");
+
+        sqlRunner.RunSql(); // We want to make RunSql READ
+    }
+}
+
+/* The output should be
+
+Loading driver
+Connecting
+Writing
+Closing
+...
+Loading driver
+Connecting
+Reading
+Closing
+
+*/
---
+++ SqlRunner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class SqlRunner
+{
+    public void RunSql()
+    {
+        LoadDriver();
+        ConnectDb();
+
+        // Our aim: to EiTHER write OR read...
+
+        Console.WriteLine("Writing");
+        Console.WriteLine("Reading");
+
+        CloseDb();
+    }
+
+    void LoadDriver()
+    {
+        Console.WriteLine("Loading driver");
+    }
+
+    void ConnectDb()
+    {
+        Console.WriteLine("Connecting");
+    }
+
+    void CloseDb()
+    {
+        Console.WriteLine("Closing");
+    }
+}
